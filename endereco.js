6fc4b974d3b8511ae7669853665791080ac15bf9
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-03-10 11:45:48 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "rua das flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + "," + numero + "-" + cidade;
+//console.log(endereco);
+console.log("o endereço correto é:" + endereco);
