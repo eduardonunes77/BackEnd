799f2dc9375db9cade4d26ab5a699e26418b6ff0
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-03-17 14:44:37 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,3 +1,11 @@
 //valores inicias do dia
 let valorPorHora = 50;
 let HorasTrabalhadas = 8;
+
+console.log("---Resumo do Exoediente---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h. `);
+
+//git add hora.js
+//git commit -m "versao2"
+//git push
+
