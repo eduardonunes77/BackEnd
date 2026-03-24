706ae33923e899bd9e64f06a803f5ceee49e2b45
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-03-24 14:40:26 +0000

    versao1

---
+++ interpolacao.js
@@ -0,0 +1,10 @@
+const nome = "EDUARDO";
+const idade = 21;
+const anoAtual = 2026;
+
+const anoNascimento = anoAtual - idade;
+const idadeEmDezAnos = idade + 10;
+
+console.log("O ano de Nascimento é: " + anoNascimento);
+console.log(`${nome} possui ${idade} anos.`);
+console.log(`Daqui a 10 anos, terá ${idadeEmDezAnos} Anos`);
