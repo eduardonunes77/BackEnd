1cf5b09fcff25c0cf6ab0f6df08d569cd3e1b00e
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-04-27 13:51:47 +0000

    v1

---
+++ sgp.js
@@ -0,0 +1,14 @@
+//SISTEMA DE GERENCIAMENTO DE PERSONAGEM
+
+function calcularPontuacaoTotal(historicoDePontos) {
+    const somarRodada = function(acumulador, pontosDaFase){
+        const novaSoma = acumulador + pontosDaFase;
+        return novaSoma;
+};
+
+const pontuacaoFinal = historicoDePontos.reduce(somarRodada, 0);
+};
+
+function filtrarMissoesConcluidas(listaDeMissoes) {
+    const verificarStatusDaMissao = function(missao) {
+        const foiFinalizada = missao.status === "concluida"
