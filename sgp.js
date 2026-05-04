d75a4c0da82ac0a05baa0efc3c6c8f51390bf9f5
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-05-04 13:56:22 +0000

    v2

--- sgp.js
+++ sgp.js
@@ -25,8 +25,24 @@ function atualizarInventario(inventarioAtual, acao, nomeDoItem){
     if (acao === "pegar"){
         const inventarioComItemNovo =[inventarioAtual, nomeDoItem];
         novoInventario = inventarioComItemNovo;
-    } else if (acao ==="descartar");
-}
+    } else if (acao ==="descartar") {
+        const inventarioSemUltimoItem = inventarioAtual.slice();
+        novoInventario = inventarioSemUltimoItem;
+    } else {
+        novoInventario = inventarioAtual;
+    }
+    return novoInventario; //Retorna a mochila do jogador atualizado
 
+};
 const pontosDoJogador = [100, 50, 200, 10];
+const total = calcularPontuacaoTotal(pontosDoJogador);
 console.log(pontosDoJogador);
+
+const missoes = [
+    {nome: "Salvar a aldeia", status:"Concluída"},
+    {nome: "Encontrar o mapa", status:"Em andamento"},
+    {nome: "Derrotar o Chefe", status:"Falhou"},
+    {nome: "Coletar recursos", status:"Concluída"},
+];
+
+console.log(missoes);
