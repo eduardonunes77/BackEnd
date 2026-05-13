8db2ccbf969fe3374168fac2a6d4ab2534bc114a
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-05-13 13:54:22 +0000

    V1

---
+++ desafio.js
@@ -0,0 +1,12 @@
+const aluno = "Eduardo";
+let idade = 19;
+const email = "eduardo77.gmail.com";
+const curso = "esportes";
+const desempenhoDoAluno = "Ensino fundamental completo, Ensino médio completo e notas acima das médias";
+let notasInicioDoAno = 8.6;
+let notasMeioDoAno = 7.0;
+let notasFinalDoAno = 9.1;
+
+console.log(`O aluno ${aluno} tem ${idade} anos, eles faz o curso de ${curso} e o desempenho dele escolar foi de: ${desempenhoDoAluno}`);
+
+let notaTotal = (notasInicioDoAno + notasMeioDoAno + notasFinalDoAno) / 2;
