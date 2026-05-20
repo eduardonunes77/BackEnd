14f9a1f9ad5f634571507ea0fd276e0dd26ba4ea
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-05-20 13:55:03 +0000

    v5

--- desafio.js
+++ desafio.js
@@ -5,16 +5,19 @@ const curso = "esportes";
 const desempenhoDoAluno = "Ensino fundamental completo, Ensino médio completo e notas acima das médias";
 
 let trabalhoNoInicioDoAno = 1.6
-let provaNoInicioDoAno = 3.0 + 4.0;
+let provaNoInicioDoAno = (3.0 + 4.0);
 let notasInicioDoAno = 8.6;
 
-let trabalhoNoMeioDoAno =
-let 
+let trabalhoNoMeioDoAno = 1.5
+let provaNoMeioDoAno = (2.5 + 3.0);
 let notasMeioDoAno = 7.0;
 
-let trabalhoNoFinalDoAno = 1.6
+let trabalhoNoFinalDoAno = 1.0;
+let provaNoFinalDoANo = (3.1 + 5.0);
 let notasFinalDoAno = 9.1;
 
-console.log(`O aluno ${aluno} tem ${idade} anos, eles faz o curso de ${curso} e o desempenho dele escolar foi de: ${desempenhoDoAluno}`);
+console.log(`O aluno ${aluno} tem ${idade} anos, ele faz o curso de ${curso} e o desempenho dele escolar foi de: ${desempenhoDoAluno}`);
 
-let notaTotal = (notasInicioDoAno + notasMeioDoAno + notasFinalDoAno) / 2;
+let notaTotal = (notasInicioDoAno + notasMeioDoAno + notasFinalDoAno) / 3;
+
+console.log(`${provaNoInicioDoAno}`)
