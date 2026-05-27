ae5790752ddda37f709b8cfa8333e68b0b502420
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-05-27 13:52:51 +0000

    V6

--- desafio.js
+++ desafio.js
@@ -16,8 +16,14 @@ let trabalhoNoFinalDoAno = 1.0;
 let provaNoFinalDoANo = (3.1 + 5.0);
 let notasFinalDoAno = 9.1;
 
-console.log(`O aluno ${aluno} tem ${idade} anos, ele faz o curso de ${curso} e o desempenho dele escolar foi de: ${desempenhoDoAluno}`);
-
 let notaTotal = (notasInicioDoAno + notasMeioDoAno + notasFinalDoAno) / 3;
 
-console.log(`${provaNoInicioDoAno}`)
+
+let diferencaNota = (notasFinalDoAno - notasMeioDoAno);
+let notasDeclaradas = "A maior nota dele foi no final do ano de 91 e a menor nota dele foi de 70, a diferença de notas foi:";
+
+let frequêcia = 94;
+
+console.log(`O aluno ${aluno} tem ${idade} anos, ele faz o curso de ${curso} e o desempenho dele escolar foi de: ${desempenhoDoAluno}`);
+console.log(`${notasDeclaradas} ${diferencaNota}`);
+console.log(`ele teve a frequência de ${frequêcia} e teve a média de ${notaTotal}`);
